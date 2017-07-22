9c9e59434e021ab34588da9ace64e340498766a5
Sergey V. Kolesnikov <sergey.kolesnikov@netcracker.com>
2017-07-22 15:30:07 +0300

    changes according https://angular.io/tutorial/toh-pt5

---
+++ src/app/heroes-list/heroes-list.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import {Hero} from '../hero';
+import {HeroService} from '../hero.service';
+
+@Component({
+  selector: 'app-heroes-list',
+  templateUrl: './heroes-list.component.html',
+  styleUrls: ['./heroes-list.component.css'],
+})
+export class HeroesListComponent implements OnInit {
+  constructor(private heroService: HeroService) {}
+
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+}
