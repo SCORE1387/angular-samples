1ca75bac874e9d7b2cfec05f86ce09cddea9dcb5
Sergey V. Kolesnikov <sergey.kolesnikov@netcracker.com>
2017-07-23 11:00:19 +0300

    changes according https://angular.io/tutorial/toh-pt6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 
 import {AppComponent} from './app.component';
 import {HeroDetailComponent} from './hero-detail/hero-detail.component';
 import {HeroesListComponent} from './heroes-list/heroes-list.component';
 import {HeroService} from './hero.service';
 import {DashboardComponent} from './dashboard/dashboard.component';
+import {InMemoryDataService} from './in-memory-data.service';
 
 import {AppRoutingModule} from './app-routing.module';
 
@@ -20,6 +24,8 @@ import {AppRoutingModule} from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   providers: [HeroService],
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,14 +1,27 @@
 import { Injectable } from '@angular/core';
-import { HEROES } from './mock-heroes';
 import { Hero } from './hero';
+import {Http} from '@angular/http';
+import 'rxjs/add/operator/toPromise';
 
 @Injectable()
 export class HeroService {
+  private heroesUrl = 'api/heroes';
+
+  constructor(private http: Http) {}
+
   getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+    return this.http.get(this.heroesUrl)
+      .toPromise()
+      .then(response => response.json().data as Hero[])
+      .catch(this.handleError);
   }
 
   getHero(id: number): Promise<Hero> {
     return this.getHeroes().then(heroes => heroes.find(hero => hero.id === id));
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('Unable to retrieve heroes.', error);
+    return Promise.reject(error.message || error);
   }
 }
